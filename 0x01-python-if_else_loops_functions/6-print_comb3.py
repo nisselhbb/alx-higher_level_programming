9129d6856d587477bc48d096d18c3caa2289a708
nisselhbb <nissrineelhobbi@gmail.com>
2023-09-05 18:25:00 -0700

    printing all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(i+1, 10):
+        among = ", "
+        if i == 8:
+            among = "\n"
+            print("{:d}{:d}".format(i, j), end=among)
