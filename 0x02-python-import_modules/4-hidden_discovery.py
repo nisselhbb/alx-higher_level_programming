8be76464dcd4b491395c658a452531ee0121dac1
nisselhbb <nissrineelhobbi@gmail.com>
2023-09-07 17:02:57 -0700

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for n in range(len(dir(hidden_4))):
-        if (dir(hidden_4)[n][2:] != "__"):
-            print(dir(hidden_4)[n])
+    names = dir(hidden_4)
+    for n in names:
+        if n[2:] != "__":
+            print(n)
