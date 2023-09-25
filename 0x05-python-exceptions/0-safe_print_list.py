8fc3823caa244dcbc9491e89ae917e41802eea66
nisselhbb <nissrineelhobbi@gmail.com>
2023-09-25 14:13:50 -0700

    writing a function that prints x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             if x >= e:
-            print(my_list[i], end=" ")
+            print(i, end=" ")
             e += 1
         print()
         return e
