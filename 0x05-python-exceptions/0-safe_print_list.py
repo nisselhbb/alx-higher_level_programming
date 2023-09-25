82fcf8498a21da99d40f675aeebed5694481468c
nisselhbb <nissrineelhobbi@gmail.com>
2023-09-25 14:20:49 -0700

    writing a function that prints x elements of a listchmod

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    e = 0
     try:
-        for i in range(x):
-            if x >= e:
-            print(i, end=" ")
-            e += 1
+        e = 0
+        for i in my_list:
+            if x > e:
+                print(i, end="")
+                e += 1
         print()
         return e
     except IndexError:
