ad379681d282e4a873a57f7a3be47621b6dc4279
nisselhbb <nissrineelhobbi@gmail.com>
2023-09-26 15:19:10 -0700

    wrinting a class Square, initiliazing its args, with a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """represents a class square"""
+    def __init__(self, size):
+        """__size: the size of the square
+        __init__(self, size): initializes a square instance
+        Args:
+            size"""
+        self.__size = size
