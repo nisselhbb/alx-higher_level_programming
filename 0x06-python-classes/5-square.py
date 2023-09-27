35b0a7a275550713f7f53e5442f6c65ea669f463
nisselhbb <nissrineelhobbi@gmail.com>
2023-09-26 17:08:19 -0700

    all what was done before + defining an instance method that prints in stdout the square with the character #

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""defines a class"""
+"""defines a class square"""
 
 
 class Square:
+    """represents a class square"""
     def __init__(self, size=0):
-        self.__size = size
-    """__size: the size of the square
+        """__size: the size of the square
     __init__(self, size): initializes a square instance
     Args:
     size"""
+        self.__size = size
 
     @property
     def size(self):
-        """getter method to retrieve the
-        size of the square"""
-        return self.__size
+        """getter method to retrive
+        the size of the square"""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
+        """sets value of the size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -25,8 +27,7 @@ class Square:
         self.__size = value
 
     def area(self):
-        """computes and returns the size of the current
-        square area"""
+        """computes and returns the current square area"""
         return self.__size ** 2
 
     def my_print(self):
