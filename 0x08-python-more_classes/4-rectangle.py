bb2135336cf22edc416771ced0362f373ebe25a6
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-02 15:04:31 -0700

    a class Rectangle that defines a rectangle by: (based on 3-rectangle.py)

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -80,4 +80,5 @@ class Rectangle:
         return rec_string
 
     def __repr__(self):
+        """returns a string representation of the object"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
