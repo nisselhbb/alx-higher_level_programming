000d27996113d8eb7b99517f7152e7408ef58a2f
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-04 02:50:09 -0700

    checking

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""a function that prints a text with 2 new lines
-    after each of these characters: ., ? and :"""
+"""5-text_indentation module"""
 
 
 def text_indentation(text):
-    """
-    prints a text with 2 new lines after each of these
-    characters . ? :
-    args:
-        text: the text to be checked
-    raises:
-        TypeError: if text is not a string
-    """
-    if not isinstance(text, str):
+    """that prints a text with 2 new lines after characters"""
+    if type(text) is not str:
         raise TypeError("text must be a string")
-    r = ""
-    flag = False
-    for c in text:
-        if c in (".", "?", ":"):
-            r += c + "\n\n"
-            flag = True
-        else:
-            r += c
-            flag = False
-    r = r.rstrip("\n")
-    print(r)
+    flag = 0
+    for a in text:
+        if flag == 0:
+            if a == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                flag = 0
+            else:
+                print(a, end="")
