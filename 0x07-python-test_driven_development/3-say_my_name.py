7ff59643959178f279000050fc858b0f595c3609
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-04 03:16:38 -0700

    adding documentation

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,7 +4,15 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """prints My name is followed by first and last"""
+    """
+    prints full name
+    args:
+        first_name: 1st name
+        last_name: the last name
+    raises:
+        TypeError: if the 1st or the last name are not
+        strings
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
