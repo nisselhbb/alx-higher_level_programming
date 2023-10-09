fb09534261761b01f5b1aa589624a0e710b28661
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-09 16:08:36 -0700

    creating Rectangle class inheriting from BaseGeometry with private width and height/ defining area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""defining a subclass Rectangle"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """the subclass Rectangle"""
+    def __init__(self, width, height):
+        """Instantiation with width and height"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """the Rectangle area"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """string showing the result of the area of the Rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
