83472f03e9e6bb0029e8f410c380fa9b9f6a927e
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-09 16:20:12 -0700

    a subclass Square that inherits from Rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,9 +8,11 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """the subclass Square"""
     def __init__(self, size):
+        """Instantiation with size"""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
+        """the Square area"""
         return self.__size ** 2
