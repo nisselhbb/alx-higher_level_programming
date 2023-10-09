133d2eb1900dc73fe43c56f3360c852b379aeccc
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-09 16:26:20 -0700

    a subclass Square that inherits from Rectangle with string representation

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""defining a subclass Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """the subclass Square"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """the Square area"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """string showing the result of teh area of the Square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
