b79e6c0c0e82a36a2afade123f9d24c4f0face2b
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-14 09:19:05 -0700

    a Rectangle class inherited from Base class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -38,7 +38,7 @@ class Rectangle(Base):
             if not isinstance(value, int):
                 raise TypeError("width must be an integer")
             elif value < 0:
-                raise ValueError("width must be >= 0")
+                raise ValueError("width must be > 0")
             self.__width = value
 
     @property
@@ -58,7 +58,7 @@ class Rectangle(Base):
             if not isinstance(value, int):
                 raise TypeError("height must be an integer")
             elif value < 0:
-                raise ValueError("height must be >= 0")
+                raise ValueError("height must be > 0")
             self.__height = value
 
     @property
