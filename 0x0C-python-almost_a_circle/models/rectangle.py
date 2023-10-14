d099138672c92c0553e98136b548e80f1fc794cc
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-14 10:54:30 -0700

    updating Rectangle by adding the area

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,80 +23,84 @@ class Rectangle(Base):
 
     @property
     def width(self):
-            """gets and returns the width of the rectangle"""
-            return self.__width
+        """gets and returns the width of the rectangle"""
+        return self.__width
 
     @width.setter
     def width(self, value):
-            """
-             sets the width of the rec
-        args: value(int): the new width of the rec
-        Raises:
-            TypeError: if the value is not an integer
-            ValueError: if the value is less than 0
-            """
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be > 0")
-            self.__width = value
+        """
+    sets the width of the rec
+    args: value(int): the new width of the rec
+    Raises:
+        TypeError: if the value is not an integer
+        ValueError: if the value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
-            """gets and returns the height of the rectangle"""
-            return self.__height
+        """gets and returns the height of the rectangle"""
+        return self.__height
 
     @height.setter
     def height(self, value):
-            """
-             sets the height of the rec
-        args: value(int): the new height of the rec
-        Raises:
-            TypeError: if the value is not an integer
-            ValueError: if the value is less than 0
-            """
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            elif value < 0:
-                raise ValueError("height must be > 0")
-            self.__height = value
+        """
+    sets the height of the rec
+    args: value(int): the new height of the rec
+    Raises:
+        TypeError: if the value is not an integer
+        ValueError: if the value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
-            """gets and returns the x coordinate of the rectangle"""
-            return self.__x
+        """gets and returns the x coordinate of the rectangle"""
+        return self.__x
 
     @x.setter
     def x(self, value):
-            """
-             sets the x coordinate of the rec
-        args: value(int): the coordinate value of the rec
-        Raises:
-            TypeError: if the value is not an integer
-            ValueError: if the value is less than 0
-            """
-            if not isinstance(value, int):
-                raise TypeError("x must be an integer")
-            elif value < 0:
-                raise ValueError("x must be >= 0")
-            self.__x = value
+        """
+    sets the x coordinate of the rec
+    args: value(int): the coordinate value of the rec
+    Raises:
+        TypeError: if the value is not an integer
+        ValueError: if the value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
-            """gets and returns the y coordinate of the rectangle"""
-            return self.__y
+        """gets and returns the y coordinate of the rectangle"""
+        return self.__y
 
     @y.setter
     def y(self, value):
-            """
-             sets the y coordinate of the rec
-        args: value(int): the coordinate value of the rec
-        Raises:
-            TypeError: if the value is not an integer
-            ValueError: if the value is less than 0
-            """
-            if not isinstance(value, int):
-                raise TypeError("y must be an integer")
-            elif value < 0:
-                raise ValueError("y must be >= 0")
-            self.__y = value
+        """
+    sets the y coordinate of the rec
+    args: value(int): the coordinate value of the rec
+    Raises:
+        TypeError: if the value is not an integer
+        ValueError: if the value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """return the area value of the Rectangle instance"""
+        return self.__height * self.__width
