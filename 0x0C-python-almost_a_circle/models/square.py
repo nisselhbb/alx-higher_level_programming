0e02df92a1b7d4fe332fd86a5bbd5500fe888c03
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-14 18:36:13 -0700

    - The constructor sets the size, x, y, and id, and it uses the superclass

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""defining a Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """an inherited class representing a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        initializes a square instance
+        size: the size of the square
+        x: x-coordinate of the square
+        y: y-coordinate of the square
+        id: the id of the square
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """returns [Square] (<id>) <x>/<y> - <size>, where in
+        this case size is whether the width or the height"""
+        one = str(f"{self.x}/{self.y}")
+        return f"[Square] ({self.id}) {one} - {self.width}"
+
+    @property
+    def size(self):
+        """gets an returns the size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """sets the value of the square"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """assigns arguments to attributes using *args
+        and **kwargs"""
+        if args:
+            attr = ["id", "size", "x", "y"]
+            for j, arg in enumerate(args):
+                setattr(self, attr[j], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
