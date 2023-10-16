f0d8bbeaa784cb03d38fef846044d8561bea3bc8
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-15 18:32:42 -0700

    adding the class method that returns a list of instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,3 +35,34 @@ class Base:
                 list_dict.append(obj.to_dictionary())
         with open(filename, "w", encoding="UTF-8") as f:
             f.write(cls.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, encoding="UTF-8") as f:
+                inst = f.read()
+                if not inst:
+                    return []
+                list_dicts = cls.from_json_string(inst)
+                return [cls.create(**di) for di in list_dicts]
+        except FileNotFoundError:
+            return []
