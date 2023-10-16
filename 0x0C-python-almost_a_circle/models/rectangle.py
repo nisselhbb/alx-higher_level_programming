1badad88cfe02d9b278ae7638f42bed45d0dcb83
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-16 10:30:44 -0700

    fixing

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -36,9 +36,9 @@ class Rectangle(Base):
         TypeError: if the value is not an integer
         ValueError: if the value is less than 0
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -56,9 +56,9 @@ class Rectangle(Base):
         TypeError: if the value is not an integer
         ValueError: if the value is less than 0
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
