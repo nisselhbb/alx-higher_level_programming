d1801d2d15fd20f5a5598db3c9a45bf94b66fed7
nisselhbb <nissrineelhobbi@gmail.com>
2023-10-16 14:33:00 -0700

    added save to/load from cvs

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """defining a the 1st class Base"""
 import csv
 import json
+import turtle
 
 
 class Base:
@@ -66,4 +67,39 @@ class Base:
                 list_dicts = cls.from_json_string(inst)
                 return [cls.create(**di) for di in list_dicts]
         except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """a class method that serializes in CSV"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, "w", newline="") as csvfile:
+            if list_objs is None or list_objs == []:
+                csvfile.write("[]")
+            else:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """a classmethod that deserializes in CVS"""
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [
+                        dict([k, int(v)] for k, v in d.items())
+                        for d in list_dicts
+                        ]
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
             return []
