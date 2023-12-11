27658656e574481444c4684ee5bc9fd71c420c95
nisselhbb <nissrineelhobbi@gmail.com>
2023-12-11 23:50:01 +0100

    making the file executable

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,4 +3,4 @@ const { argv } = require('process');
 const first = argv[2];
 const second = argv[3];
 const isString = ' is ';
-console.log(firstArgv + middleString + secondArgv);
+console.log(first + isString + second);
