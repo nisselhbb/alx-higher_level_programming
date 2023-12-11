3b539eddb176cffd5bfdcb4ec19db529b1535fc2
nisselhbb <nissrineelhobbi@gmail.com>
2023-12-11 23:31:09 +0100

    making the files executable

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const var1 = "C is fun";
-const var2 = "Python is cool";
-const var3 = "javaScript is amazing";
+const var1 = 'C is fun';
+const var2 = 'Python is cool';
+const var3 = 'javaScript is amazing';
 console.log(var1 + '\n' + var2 + '\n' + var3);
